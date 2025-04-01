8cbb9973a16535c26ee7fef2e299caa564ed2002
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-04-01 00:56:09 -1100

    add frontend url

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ const mode = process.env.mode
 const server = http.createServer(app)
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app/'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 const io = socket(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app/'],
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app'],
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     }
 });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
