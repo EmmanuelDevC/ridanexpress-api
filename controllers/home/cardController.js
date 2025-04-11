57ef91ea5e8c5eabde3685ce0e89bcdc02ace4b4
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-04-11 12:26:59 -1100

    Dashboard deployment

--- controllers/home/cardController.js
+++ controllers/home/cardController.js
@@ -10,46 +10,6 @@ const {
     }
 } = require('mongoose')
 class cardController {
-    // add_to_card = async (req, res) => {
-    //     const {
-    //         userId,
-    //         productId,
-    //         quantity
-    //     } = req.body
-    //     try {
-    //         const product = await cardModel.findOne({
-    //             $and: [{
-    //                 productId: {
-    //                     $eq: productId
-    //                 }
-    //             },
-    //             {
-    //                 userId: {
-    //                     $eq: userId
-    //                 }
-    //             }
-    //             ]
-    //         })
-    //         if (product) {
-    //             responseReturn(res, 404, {
-    //                 error: 'Product already added to card'
-    //             })
-    //         } else {
-    //             const product = await cardModel.create({
-    //                 userId,
-    //                 productId,
-    //                 quantity
-    //             })
-    //             responseReturn(res, 201, {
-    //                 message: 'Add to card success',
-    //                 product
-    //             })
-    //         }
-    //     } catch (error) {
-    //         console.log(error.message)
-    //     }
-    // }
-
     add_to_card = async (req, res) => {
         const { userId, productId, quantity } = req.body;
 
--- server.js
+++ server.js
@@ -13,14 +13,14 @@ const mode = process.env.mode
 const server = http.createServer(app)
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app', 'https://ridan-express-dashboard.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 const io = socket(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app'],
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ridan-express-client.vercel.app', 'https://ridan-express-dashboard.vercel.app'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     }
