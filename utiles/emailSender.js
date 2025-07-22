fd0026b18657794298aa984850007e4a62d4c414
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-07-22 22:58:46 +0100

    backend commit

--- utiles/emailSender.js
+++ utiles/emailSender.js
@@ -46,7 +46,7 @@ const sendVerificationEmail = async (email, token) => {
                 </p>
 
                 <div style="text-align: center; margin: 40px 0;">
-                    <a href="https://martafrik.vercel.app/verify-email?token=${token}" 
+                    <a href="https://ridanexpress.vercel.app/verify-email?token=${token}" 
                        style="
                            background-color: #3498db;
                            color: white;
